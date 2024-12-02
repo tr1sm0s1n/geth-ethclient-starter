c4902caf3d6fec742aeedf70bd9636260f349244
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-12-02 23:06:31 +0530

    🔧 | update auth generator to return private key

--- cmd/contracts/deploy.go
+++ cmd/contracts/deploy.go
@@ -30,7 +30,7 @@ func main() {
 }
 
 func deployContract(client *ethclient.Client) (common.Address, *types.Transaction, error) {
-	auth, err := helpers.AuthGenerator(client, "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+	auth, _, err := helpers.AuthGenerator(client, "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
 	if err != nil {
 		return common.Address{}, nil, err
 	}
--- helpers/auth.go
+++ helpers/auth.go
@@ -11,38 +11,38 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func AuthGenerator(client *ethclient.Client, key string) (*bind.TransactOpts, error) {
+func AuthGenerator(client *ethclient.Client, key string) (*bind.TransactOpts, *ecdsa.PrivateKey, error) {
 	privateKey, err := crypto.HexToECDSA(key[2:])
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		return nil, errors.New("error casting public key to ECDSA")
+		return nil, nil, errors.New("error casting public key to ECDSA")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	gasLimit := uint64(927000)
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	chainID, err := client.ChainID(context.Background())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	auth.Nonce = big.NewInt(int64(nonce))
@@ -50,5 +50,5 @@ func AuthGenerator(client *ethclient.Client, key string) (*bind.TransactOpts, er
 	auth.GasLimit = gasLimit
 	auth.GasPrice = gasPrice
 
-	return auth, nil
+	return auth, privateKey, nil
 }
