bc982f059747fe3d05f63f7994e17973624204c9
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-01-30 21:21:18 +0530

    🔔 | use NewCertFilterer, WatchIssued from binding for listening

--- cmd/events/listener.go
+++ cmd/events/listener.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/tr1sm0s1n/geth-ethclient-starter/contract"
 	"github.com/tr1sm0s1n/geth-ethclient-starter/helpers"
-	"os"
-	"strings"
-
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 )
 
 func main() {
@@ -22,17 +17,13 @@ func main() {
 
 	contractAddress := common.HexToAddress(os.Getenv("CONTRACT_ADDRESS"))
 
-	query := ethereum.FilterQuery{
-		Addresses: []common.Address{contractAddress},
-	}
-
-	certABI, err := abi.JSON(strings.NewReader(contract.CertMetaData.ABI))
+	filterer, err := contract.NewCertFilterer(contractAddress, client)
 	if err != nil {
 		panic(err)
 	}
 
-	logs := make(chan types.Log)
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	logs := make(chan *contract.CertIssued)
+	sub, err := filterer.WatchIssued(nil, logs)
 	if err != nil {
 		panic(err)
 	}
@@ -44,17 +35,14 @@ func main() {
 		select {
 		case err := <-sub.Err():
 			panic(err)
-		case vLog := <-logs:
-			var IssuedEvent contract.CertIssued
-
-			certABI.UnpackIntoInterface(&IssuedEvent, "Issued", vLog.Data)
-			uLog, _ := vLog.MarshalJSON()
+		case log := <-logs:
+			rw, _ := log.Raw.MarshalJSON()
 			fmt.Println("Certificate issued!!")
 			fmt.Println("--------------------")
-			fmt.Printf("Course: \033[34m%s\033[0m\n", IssuedEvent.Course)
-			fmt.Printf("ID: \033[34m%d\033[0m\n", IssuedEvent.Id.Int64())
-			fmt.Printf("Date: \033[34m%s\033[0m\n", IssuedEvent.Date)
-			fmt.Printf("Raw log: \033[32m%s\033[0m\n", string(uLog))
+			fmt.Printf("Course: \033[34m%s\033[0m\n", log.Course)
+			fmt.Printf("ID: \033[34m%d\033[0m\n", log.Id.Int64())
+			fmt.Printf("Date: \033[34m%s\033[0m\n", log.Date)
+			fmt.Printf("Raw log: \033[32m%s\033[0m\n", string(rw))
 			fmt.Println("--------------------")
 		}
 	}
